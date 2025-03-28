11fbdd6f7c834d8971bd7a980cae08d046b78ef4
wanjiku8 <wanjikuf141@gmail.com>
2025-03-28 00:33:06 -0700

    commit

--- server/app.py
+++ server/app.py
@@ -1,19 +1,17 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
-
 
 @app.route('/')
 def index():
@@ -22,6 +20,27 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    
+    if not earthquake:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+    
+    return jsonify(earthquake.to_dict()),200
+    
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    quakes_data = [quake.to_dict() for quake in quakes]
+    
+    return jsonify({
+        "count": len(quakes_data),
+        "quakes": quakes_data
+    })
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
